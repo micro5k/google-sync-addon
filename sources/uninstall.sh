40e57a00486578aaafc84009aa956e04208f54f2
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-26 20:30:21 +0200

    Improved microG / GApps uninstaller

--- sources/uninstall.sh
+++ sources/uninstall.sh
@@ -17,11 +17,17 @@ DELETE_LIST="
 ${PRIVAPP_PATH}/GoogleBackupTransport/
 ${PRIVAPP_PATH}/GoogleBackupTransport.apk
 
-${SYS_PATH}/app/GoogleCalendarSyncAdapter/
 ${SYS_PATH}/app/GoogleContactsSyncAdapter/
-
-${SYS_PATH}/app/GoogleCalendarSyncAdapter.apk
 ${SYS_PATH}/app/GoogleContactsSyncAdapter.apk
+
+${SYS_PATH}/app/GoogleCalendarSyncAdapter/
+${SYS_PATH}/app/GoogleCalendarSyncAdapter.apk
+
+${SYS_PATH}/app/IchnaeaNlpBackend/
+${SYS_PATH}/app/IchnaeaNlpBackend.apk
+
+${SYS_PATH}/app/NominatimGeocoderBackend/
+${SYS_PATH}/app/NominatimGeocoderBackend.apk
 "
 
 rm -rf ${DELETE_LIST}  # Filenames cannot contain spaces
