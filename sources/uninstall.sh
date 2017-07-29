9149f959ab2edbdb249a54361326ba4be3024963
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-29 23:45:19 +0200

    Improved Google Sync Add-on / GApps uninstaller

--- sources/uninstall.sh
+++ sources/uninstall.sh
@@ -14,14 +14,31 @@ if [[ -z "$INSTALLER" ]]; then
 fi
 
 DELETE_LIST="
+${SYS_PATH}/etc/default-permissions/google-sync-permissions.xml
+
 ${PRIVAPP_PATH}/GoogleBackupTransport/
 ${PRIVAPP_PATH}/GoogleBackupTransport.apk
+${PRIVAPP_PATH}/GoogleBackupTransport.odex
+
+${PRIVAPP_PATH}/GoogleContactsSyncAdapter/
+${PRIVAPP_PATH}/GoogleContactsSyncAdapter.apk
+${PRIVAPP_PATH}/GoogleContactsSyncAdapter.odex
+
+${PRIVAPP_PATH}/GoogleCalendarSyncAdapter/
+${PRIVAPP_PATH}/GoogleCalendarSyncAdapter.apk
+${PRIVAPP_PATH}/GoogleCalendarSyncAdapter.odex
+
+${SYS_PATH}/app/GoogleBackupTransport/
+${SYS_PATH}/app/GoogleBackupTransport.apk
+${SYS_PATH}/app/GoogleBackupTransport.odex
 
 ${SYS_PATH}/app/GoogleContactsSyncAdapter/
 ${SYS_PATH}/app/GoogleContactsSyncAdapter.apk
+${SYS_PATH}/app/GoogleContactsSyncAdapter.odex
 
 ${SYS_PATH}/app/GoogleCalendarSyncAdapter/
 ${SYS_PATH}/app/GoogleCalendarSyncAdapter.apk
+${SYS_PATH}/app/GoogleCalendarSyncAdapter.odex
 "
 
 rm -rf ${DELETE_LIST}  # Filenames cannot contain spaces
