bf735648729f4547aefc51ec86b9aaadfe1652cb
ale5000 <ale5000-git@users.noreply.github.com>
2017-12-09 14:45:49 +0100

    Sync with microG unofficial installer

--- zip-content/inc/common.sh
+++ zip-content/inc/common.sh
@@ -2,20 +2,20 @@
 
 <<LICENSE
   Copyright (C) 2016-2017  ale5000
-  This file is part of Google Sync Add-on by @ale5000.
+  This file was created by ale5000 (ale5000-git on GitHub).
 
-  Google Sync Add-on is free software: you can redistribute it and/or modify
+  This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version, w/Google Sync Add-on zip exception.
+  (at your option) any later version, w/ zip exception.
 
-  Google Sync Add-on is distributed in the hope that it will be useful,
+  This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Google Sync Add-on.  If not, see <http://www.gnu.org/licenses/>.
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE
 
 ### GLOBAL VARIABLES ###
