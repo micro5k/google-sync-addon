93565f8c6d65fc15f039705c3dda3d4d8a9fa94a
ale5000 <ale5000-git@users.noreply.github.com>
2018-12-09 17:19:32 +0100

    Fixed errors during sync

--- conf.sh
+++ conf.sh
@@ -11,10 +11,10 @@ EOF
 files_to_download()
 {
 cat <<EOF
-GoogleContactsSyncAdapter.apk|zip-content/files/app|c46d9bbe31f85a5263eb6a2a0932abbf9ac3ecc9|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=290062
-GoogleCalendarSyncAdapter.apk|zip-content/files/app|aa482580c87a43c83882c05a4757754917d47f32|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=72565
-GoogleBackupTransport.apk|zip-content/files/priv-app-4.4|6f186d368014022b0038ad2f5d8aa46bb94b5c14|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=152392
-GoogleContactsSyncAdapter.apk|zip-content/files/app-4.4|68597be59f16d2e26a79def6fa20bc85d1d2c3b3|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=152374
-GoogleCalendarSyncAdapter.apk|zip-content/files/app-4.4|cf9fa487dfe0ead8576d6af897687e7fa2ae00fa|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=99188
+GoogleContactsSyncAdapter.apk|files/app|c46d9bbe31f85a5263eb6a2a0932abbf9ac3ecc9|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=290062
+GoogleCalendarSyncAdapter.apk|files/app|aa482580c87a43c83882c05a4757754917d47f32|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=72565
+GoogleBackupTransport.apk|files/priv-app-4.4|6f186d368014022b0038ad2f5d8aa46bb94b5c14|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=152392
+GoogleContactsSyncAdapter.apk|files/app-4.4|68597be59f16d2e26a79def6fa20bc85d1d2c3b3|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=152374
+GoogleCalendarSyncAdapter.apk|files/app-4.4|cf9fa487dfe0ead8576d6af897687e7fa2ae00fa|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=99188
 EOF
 }
