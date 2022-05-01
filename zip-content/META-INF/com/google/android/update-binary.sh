2004fb041f3b939114f75d234e09a802d5ec173d
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-05-01 14:54:18 +0200

    Sync with microG unofficial installer

--- zip-content/META-INF/com/google/android/update-binary.sh
+++ zip-content/META-INF/com/google/android/update-binary.sh
@@ -1,32 +1,22 @@
 #!/sbin/sh
+# shellcheck disable=SC3043
+# SC3043: In POSIX sh, local is undefined
 
-<<LICENSE
-  Copyright (C) 2016-2018  ale5000
-  This file was created by ale5000 (ale5000-git on GitHub).
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version, w/ zip exception.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-LICENSE
+# SPDX-FileCopyrightText: (c) 2016-2019, 2021 ale5000
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileType: SOURCE
 
 ### GLOBAL VARIABLES ###
 
 export DEBUG_LOG=0
-export RECOVERY_API_VER="$1"
-export RECOVERY_PIPE="/proc/self/fd/$2"
-export ZIP_FILE="$3"
-export ZIP_PATH=$(dirname "$ZIP_FILE")
-BASE_TMP_PATH='/tmp'
-TMP_PATH='/tmp/custom-setup-a5k'
+export RECOVERY_API_VER="${1}"
+export OUTFD="${2}"
+export RECOVERY_PIPE="/proc/self/fd/${2}"
+export ZIP_FILE="${3}"
+ZIP_PATH="$(dirname "${ZIP_FILE:?}")"
+export ZIP_PATH
+BASE_TMP_PATH="${TMPDIR:-/tmp}"
+TMP_PATH="${TMPDIR:-/tmp}/custom-setup-a5k"
 
 MANUAL_TMP_MOUNT=0
 GENER_ERROR=0
@@ -41,43 +31,51 @@ export BOOTMODE=false
 DEBUG_LOG_ENABLED=0
 enable_debug_log()
 {
-  if [ "$DEBUG_LOG_ENABLED" -eq 1 ]; then return; fi
-  exec 3>&1 4>&2  # Backup stdout and stderr
-  exec 1>>"$ZIP_PATH/debug-a5k.log" 2>&1
+  if test "${DEBUG_LOG_ENABLED}" -eq 1; then return; fi
   DEBUG_LOG_ENABLED=1
+  exec 3>&1 4>&2  # Backup stdout and stderr
+  exec 1>>"${ZIP_PATH}/debug-a5k.log" 2>&1
 }
 
 disable_debug_log()
 {
-  if [ "$DEBUG_LOG_ENABLED" -eq 0 ]; then return; fi
-  exec 1>&3 2>&4  # Restore stdout and stderr
+  if test "${DEBUG_LOG_ENABLED}" -eq 0; then return; fi
   DEBUG_LOG_ENABLED=0
+  exec 1>&3 2>&4  # Restore stdout and stderr
 }
 
 _show_text_on_recovery()
 {
-  echo -e "ui_print $1\nui_print" >> $RECOVERY_PIPE
+  if test -e "${RECOVERY_PIPE}"; then
+    printf "ui_print %s\nui_print \n" "${1}" >> "${RECOVERY_PIPE}"
+  else
+    printf "ui_print %s\nui_print \n" "${1}" 1>&"${OUTFD}"
+  fi
 }
 
 ui_error()
 {
   ERROR_CODE=79
   if test -n "$2"; then ERROR_CODE="$2"; fi
-  >&2 echo "ERROR ${ERROR_CODE}: $1"
+  echo "ERROR ${ERROR_CODE}: $1" >&2
   _show_text_on_recovery "ERROR: $1"
   exit "${ERROR_CODE}"
 }
 
 ui_warning()
 {
-  >&2 echo "WARNING: $1"
+  echo "WARNING: $1" >&2
   _show_text_on_recovery "WARNING: $1"
 }
 
 ui_msg()
 {
-  echo -e "ui_print $1\nui_print" >> $RECOVERY_PIPE
-  if [ "$DEBUG_LOG" -ne 0 ]; then echo "$1"; fi
+  if test "${DEBUG_LOG}" -ne 0; then echo "$1"; fi
+  if test -e "${RECOVERY_PIPE}"; then
+    printf "ui_print %s\nui_print \n" "${1}" >> "${RECOVERY_PIPE}"
+  else
+    printf "ui_print %s\nui_print \n" "${1}" 1>&"${OUTFD}"
+  fi
 }
 
 ui_debug()
@@ -88,99 +86,64 @@ ui_debug()
 is_mounted()
 {
   case $(mount) in
-    *" $1 "*) return 0;;  # Mounted
+    *[[:blank:]]"$1"[[:blank:]]*) return 0;;  # Mounted
+    *)                                        # NOT mounted
   esac
   return 1  # NOT mounted
-}
-
-ensure_system_is_mounted()
-{
-  if ! is_mounted '/system'; then
-    mount '/system'
-    if ! is_mounted '/system'; then ui_error '/system cannot be mounted'; fi
-  fi
-  return 0;  # OK
-}
-
-parse_file()
-{
-  local tmp=$(grep -F "${2}=" "$1" | head -n1 | cut -d '=' -f 2) && echo "$tmp"
-  if test -n "$tmp"; then return 0; fi  # Found
-  return 1  # NOT found
-}
-
-rec_getprop()
-{
-  local prop=''
-  (test -e '/sbin/getprop' && prop=$(/sbin/getprop "ro.$1") && test -n "$prop" && echo "$prop") || (prop=$(parse_file '/default.prop' "ro.$1") && echo "$prop") || (ensure_system_is_mounted && test -e '/system/build.prop' && prop=$(parse_file '/system/build.prop' "ro.$1") && echo "$prop")
-}
-
-get_rec_abilist()
-{
-  local abilist=$(rec_getprop 'product.cpu.abilist')
-  if test -n "$abilist"; then echo "$abilist"; return 0; fi  # Found
-
-  local abi1=$(rec_getprop 'product.cpu.abi')
-  local abi2=$(rec_getprop 'product.cpu.abi2')
-  if test -n "$abi1" || test -n "$abi2"; then echo "$abi1,$abi2"; return 0; fi  # Found
-
-  return 1  # NOT found
-}
-
-is_substring()
-{
-  case "$2" in 
-    *"$1"*) return 0;;  # Found
-  esac;
-  return 1  # NOT found
 }
 
 set_perm()
 {
   local uid="$1"; local gid="$2"; local mod="$3"
   shift 3
-  chown $uid.$gid "$@"; chown $uid:$gid "$@"
-  chmod $mod "$@" || ui_error "chmod failed on '$@'" 81
+  if test "${TEST_INSTALL:-false}" = 'false'; then
+    chown "${uid}:${gid}" "$@" || chown "${uid}.${gid}" "$@" || ui_error "chown failed on: $*" 81
+  fi
+  chmod "${mod}" "$@" || ui_error "chmod failed on: $*" 81
 }
 
 set_perm_safe()
 {
   local uid="$1"; local gid="$2"; local mod="$3"
   shift 3
-  "$BASE_TMP_PATH/busybox" chown $uid:$gid "$@" || ui_error "chown failed on '$@'" 81
-  "$BASE_TMP_PATH/busybox" chmod $mod "$@" || ui_error "chmod failed on '$@'" 81
+  if test "${TEST_INSTALL:-false}" = 'false'; then
+    "${OUR_BB}" chown "${uid}:${gid}" "$@" || "${OUR_BB}" chown "${uid}.${gid}" "$@" || ui_error "chown failed on: $*" 81
+  fi
+  "${OUR_BB}" chmod "${mod}" "$@" || ui_error "chmod failed on: $*" 81
 }
 
 package_extract_file()
 {
-  unzip -opq "$ZIP_FILE" "$1" > "$2" || ui_error "Failed to extract the file '$1' from this archive" 82
+  unzip -opq "${ZIP_FILE}" "$1" > "$2" || ui_error "Failed to extract the file '$1' from this archive" 82
+  if ! test -e "$2"; then ui_error "Failed to extract the file '$1' from this archive" 82; fi
 }
 
 package_extract_file_safe()
 {
-  "$BASE_TMP_PATH/busybox" unzip -opq "$ZIP_FILE" "$1" > "$2" || ui_error "Failed to extract the file '$1' from this archive" 83
+  "${OUR_BB}" unzip -opq "${ZIP_FILE}" "$1" > "$2" || ui_error "Failed to extract the file '$1' from this archive" 83
+  if ! test -e "$2"; then ui_error "Failed to extract the file '$1' from this archive" 83; fi
 }
 
 create_dir()
 {
-  mkdir -p "$1" || ui_error "Failed to create the dir '$dir'" 84
+  mkdir -p "$1" || ui_error "Failed to create the dir: $1" 84
   set_perm 0 0 0755 "$1"
 }
 
 create_dir_safe()
 {
-  "$BASE_TMP_PATH/busybox" mkdir -p "$1" || ui_error "Failed to create the dir '$dir'" 84
+  "${OUR_BB}" mkdir -p "$1" || ui_error "Failed to create the dir: $1" 84
   set_perm_safe 0 0 0755 "$1"
 }
 
 delete_safe()
 {
-  "$BASE_TMP_PATH/busybox" rm -f "$@" || ui_error "Failed to delete files" 85
+  "${OUR_BB}" rm -f "$@" || ui_error "Failed to delete files" 85
 }
 
 delete_recursive_safe()
 {
-  "$BASE_TMP_PATH/busybox" rm -rf "$@" || ui_error "Failed to delete files/folders" 86
+  "${OUR_BB}" rm -rf "$@" || ui_error "Failed to delete files/folders" 86
 }
 
 # Input related functions
@@ -202,16 +165,16 @@ choose_timeout()
 {
   local key_code=1
   timeout -t "$1" keycheck; key_code="$?"  # Timeout return 127 when it cannot execute the binary
-  if test "$key_code" -eq 143; then
+  if test "${key_code}" -eq 143; then
     ui_msg 'Key code: No key pressed'
     return 0
-  elif test "$key_code" -eq 127 || test "$key_code" -eq 132; then
+  elif test "${key_code}" -eq 127 || test "${key_code}" -eq 132; then
     ui_msg 'WARNING: Key detection failed'
     return 1
   fi
 
-  ui_msg "Key code: $key_code"
-  check_key "$key_code"
+  ui_msg "Key code: ${key_code}"
+  check_key "${key_code}"
   return "$?"
 }
 
@@ -222,15 +185,15 @@ choose()
   ui_msg "$2"
   ui_msg "$3"
   keycheck; key_code="$?"
-  ui_msg "Key code: $key_code"
-  check_key "$key_code"
+  ui_msg "Key code: ${key_code}"
+  check_key "${key_code}"
   return "$?"
 }
 
 
 ### CODE ###
 
-test "$DEBUG_LOG" -eq 1 && enable_debug_log  # Enable file logging if needed
+test "${DEBUG_LOG}" -eq 1 && enable_debug_log  # Enable file logging if needed
 
 ui_debug 'PRELOADER'
 if ! is_mounted '/tmp'; then
@@ -244,74 +207,98 @@ if ! is_mounted '/tmp'; then
   if ! is_mounted '/tmp'; then ui_error '/tmp is NOT mounted'; fi
 fi
 
-ABI_LIST=",$(get_rec_abilist)," || ui_error "Empty ABI list"
+detect_recovery_arch()
+{
+  case "$(uname -m)" in
+    x86_64                    ) RECOVERY_ARCH='x86_64';;
+    x86 | i686                ) RECOVERY_ARCH='x86';;
+    aarch64 | arm64* | armv8* ) RECOVERY_ARCH='arm64-v8a';;
+    armv7*                    ) RECOVERY_ARCH='armeabi-v7a';;
+    armv6* | armv5*           ) RECOVERY_ARCH='armeabi';;
+    *) ui_error "Unsupported architecture: $(uname -m || true)"
+  esac
+}
+detect_recovery_arch
 
-if is_substring ',x86_64,' "$ABI_LIST"; then
+OUR_BB="${BASE_TMP_PATH}/busybox"
+if test -n "${CUSTOM_BUSYBOX:-}" && test -e "${CUSTOM_BUSYBOX}"; then
+  OUR_BB="${CUSTOM_BUSYBOX}"
+  ui_debug "Using custom BusyBox... '${OUR_BB}'"
+elif test "${RECOVERY_ARCH}" = 'x86_64'; then
   ui_debug 'Extracting 64-bit x86 BusyBox...'
-  package_extract_file 'misc/busybox/busybox-x86_64' "$BASE_TMP_PATH/busybox"
-elif is_substring ',x86,' "$ABI_LIST"; then
+  package_extract_file 'misc/busybox/busybox-x86_64.bin' "${OUR_BB}"
+elif test "${RECOVERY_ARCH}" = 'x86'; then
   ui_debug 'Extracting x86 BusyBox...'
-  package_extract_file 'misc/busybox/busybox-x86' "$BASE_TMP_PATH/busybox"
-elif is_substring ',arm64-v8a,' "$ABI_LIST"; then
+  package_extract_file 'misc/busybox/busybox-x86.bin' "${OUR_BB}"
+elif test "${RECOVERY_ARCH}" = 'arm64-v8a'; then
   ui_debug 'Extracting 64-bit ARM BusyBox...'
-  package_extract_file 'misc/busybox/busybox-arm64' "$BASE_TMP_PATH/busybox"
-  package_extract_file 'misc/keycheck/keycheck-arm' "$BASE_TMP_PATH/keycheck"
-elif is_substring ',armeabi,' "$ABI_LIST" || is_substring ',armeabi-v7a,' "$ABI_LIST"; then
+  package_extract_file 'misc/busybox/busybox-arm64.bin' "${OUR_BB}"
+  package_extract_file 'misc/keycheck/keycheck-arm' "${BASE_TMP_PATH}/keycheck"
+elif test "${RECOVERY_ARCH}" = 'armeabi-v7a' || test "${RECOVERY_ARCH}" = 'armeabi'; then
   ui_debug 'Extracting ARM BusyBox...'
-  package_extract_file 'misc/busybox/busybox-arm' "$BASE_TMP_PATH/busybox"
-  package_extract_file 'misc/keycheck/keycheck-arm' "$BASE_TMP_PATH/keycheck"
-else
-  ui_error "Unsupported CPU, ABI list: ${ABI_LIST}"
+  package_extract_file 'misc/busybox/busybox-arm.bin' "${OUR_BB}"
+  package_extract_file 'misc/keycheck/keycheck-arm' "${BASE_TMP_PATH}/keycheck"
 fi
-# Give execution rights
-chmod +x "$BASE_TMP_PATH/busybox" || ui_error "chmod failed on '$BASE_TMP_PATH/busybox'" 81  # Needed to make working the "safe" functions
-set_perm_safe 0 0 0755 "$BASE_TMP_PATH/busybox"
+if ! test -e "${OUR_BB}"; then ui_error 'BusyBox not found'; fi
+
+# Give execution rights (if needed)
+if test -z "${CUSTOM_BUSYBOX:-}" || test "${OUR_BB}" != "${CUSTOM_BUSYBOX}"; then
+  chmod +x "${OUR_BB}" || ui_error "chmod failed on '${OUR_BB}'" 81  # Needed to make working the "safe" functions
+  set_perm 0 0 0755 "${OUR_BB}"
+fi
 
 # Delete previous traces
-delete_recursive_safe "$TMP_PATH"
-create_dir_safe "$TMP_PATH"
-create_dir_safe "$TMP_PATH/bin"
+delete_recursive_safe "${TMP_PATH}"
+create_dir_safe "${TMP_PATH}"
+create_dir_safe "${TMP_PATH}/bin"
 
 PREVIOUS_PATH="${PATH}"
+DEVICE_MOUNT="$(which mount)"
+export DEVICE_MOUNT
 
-# Clean search path so only internal BusyBox applets will be used
-export PATH="$TMP_PATH/bin"
+if test "${TEST_INSTALL:-false}" = 'false'; then
+  # Clean search path so only internal BusyBox applets will be used
+  export PATH="${TMP_PATH}/bin"
 
-# Temporarily setup BusyBox
-"$BASE_TMP_PATH/busybox" --install -s "$TMP_PATH/bin"
+  # Temporarily setup BusyBox
+  "${OUR_BB}" --install -s "${TMP_PATH}/bin" || ui_error "Failed to setup BusyBox"
+fi
 
 # Temporarily setup Keycheck
-if test -e "$BASE_TMP_PATH/keycheck"; then
-  "$BASE_TMP_PATH/busybox" mv -f "$BASE_TMP_PATH/keycheck" "$TMP_PATH/bin/keycheck" || ui_error "Failed to move keycheck to the bin folder"
+if test -e "${BASE_TMP_PATH}/keycheck"; then
+  "${OUR_BB}" mv -f "${BASE_TMP_PATH}/keycheck" "${TMP_PATH}/bin/keycheck" || ui_error "Failed to move keycheck to the bin folder"
   # Give execution rights
-  set_perm_safe 0 0 0755 "$TMP_PATH/bin/keycheck"
+  set_perm_safe 0 0 0755 "${TMP_PATH}/bin/keycheck"
   KEYCHECK_ENABLED=true
 fi
 
 # Extract scripts
 ui_debug 'Extracting scripts...'
-create_dir_safe "$TMP_PATH/inc"
-package_extract_file_safe 'inc/common.sh' "$TMP_PATH/inc/common.sh"
-package_extract_file_safe 'uninstall.sh' "$TMP_PATH/uninstall.sh"
-package_extract_file_safe 'install.sh' "$TMP_PATH/install.sh"
+create_dir_safe "${TMP_PATH}/inc"
+package_extract_file_safe 'inc/common.sh' "${TMP_PATH}/inc/common.sh"
+package_extract_file_safe 'uninstall.sh' "${TMP_PATH}/uninstall.sh"
+package_extract_file_safe 'install.sh' "${TMP_PATH}/install.sh"
 # Give execution rights
-set_perm_safe 0 0 0755 "$TMP_PATH/inc/common.sh"
-set_perm_safe 0 0 0755 "$TMP_PATH/uninstall.sh"
-set_perm_safe 0 0 0755 "$TMP_PATH/install.sh"
+set_perm_safe 0 0 0755 "${TMP_PATH}/inc/common.sh"
+set_perm_safe 0 0 0755 "${TMP_PATH}/uninstall.sh"
+set_perm_safe 0 0 0755 "${TMP_PATH}/install.sh"
 
-package_extract_file_safe 'settings.conf' "$TMP_PATH/default-settings.conf"
-. "$TMP_PATH/default-settings.conf"
-test "$DEBUG_LOG" -eq 1 && enable_debug_log  # Enable file logging if needed
+package_extract_file_safe 'settings.conf' "${TMP_PATH}/default-settings.conf"
+# shellcheck source=SCRIPTDIR/../../../../settings.conf
+. "${TMP_PATH}/default-settings.conf"
+test "${DEBUG_LOG}" -eq 1 && enable_debug_log  # Enable file logging if needed
 
 # If the debug log was enabled at startup (not in the settings or in the live setup) we cannot allow overriding it from the settings
-if [ "$DEBUG_LOG_ENABLED" -eq 1 ]; then export DEBUG_LOG=1; fi
+if [ "${DEBUG_LOG_ENABLED}" -eq 1 ]; then export DEBUG_LOG=1; fi
 
 # Detect boot mode
+# shellcheck disable=SC2009
 (ps | grep zygote | grep -v grep >/dev/null) && BOOTMODE=true
-$BOOTMODE || (ps -A 2>/dev/null | grep zygote | grep -v grep >/dev/null && BOOTMODE=true)
+# shellcheck disable=SC2009
+"${BOOTMODE}" || (ps -A 2>/dev/null | grep zygote | grep -v grep >/dev/null && BOOTMODE=true)
 
 # Live setup
-if $KEYCHECK_ENABLED && [ "$LIVE_SETUP" -eq 0 ] && [ "${LIVE_SETUP_TIMEOUT}" -ge 1 ]; then
+if "${KEYCHECK_ENABLED}" && [ "${LIVE_SETUP}" -eq 0 ] && [ "${LIVE_SETUP_TIMEOUT}" -ge 1 ]; then
   ui_msg '---------------------------------------------------'
   ui_msg 'INFO: Select the VOLUME + key to enable live setup.'
   ui_msg "Waiting input for ${LIVE_SETUP_TIMEOUT} seconds..."
@@ -319,29 +306,31 @@ if $KEYCHECK_ENABLED && [ "$LIVE_SETUP" -eq 0 ] && [ "${LIVE_SETUP_TIMEOUT}" -ge
   if test "$?" -eq 3; then export LIVE_SETUP=1; fi
 fi
 
-if [ "$LIVE_SETUP" -eq 1 ]; then
+if [ "${LIVE_SETUP}" -eq 1 ]; then
   ui_msg 'LIVE SETUP ENABLED!'
-  if [ "$DEBUG_LOG" -eq 0 ]; then
+  if [ "${DEBUG_LOG}" -eq 0 ]; then
     choose 'Do you want to enable the debug log?' '+) Yes' '-) No'; if [ "$?" -ne 2 ]; then export DEBUG_LOG=1; enable_debug_log; fi
   fi
 fi
 
 ui_msg ''
 ui_debug 'Starting installation script...'
-"$BASE_TMP_PATH/busybox" bash -c "'$TMP_PATH/install.sh' Preloader '$TMP_PATH'"; STATUS="$?"
+"${OUR_BB}" ash "${TMP_PATH}/install.sh" Preloader "${TMP_PATH}"; STATUS="$?"
 
-test -f "$TMP_PATH/installed" || GENER_ERROR=1
+test -f "${TMP_PATH}/installed" || GENER_ERROR=1
 
 export PATH="${PREVIOUS_PATH}"
-delete_recursive_safe "$TMP_PATH"
+delete_recursive_safe "${TMP_PATH}"
 
 #!!! UNSAFE ENVIRONMENT FROM HERE !!!#
 
-test "$DEBUG_LOG" -eq 1 && disable_debug_log  # Disable debug log and restore normal output
+test "${DEBUG_LOG}" -eq 1 && disable_debug_log  # Disable debug log and restore normal output
 
-test "$MANUAL_TMP_MOUNT" -ne 0 && unmount_safe '/tmp'
+if test "${MANUAL_TMP_MOUNT}" -ne 0; then
+  "${OUR_BB}" umount '/tmp' || ui_error "Failed to unmount '/tmp'"
+fi
 
-if test "$STATUS" -ne 0; then ui_error "Installation script failed with error $STATUS" "$STATUS"; fi
-if test "$GENER_ERROR" -ne 0; then ui_error 'Installation failed with an unknown error'; fi
+if test "${STATUS}" -ne 0; then ui_error "Installation script failed with error ${STATUS}" "${STATUS}"; fi
+if test "${GENER_ERROR}" -ne 0; then ui_error 'Installation failed with an unknown error'; fi
 
-delete_safe "$BASE_TMP_PATH/busybox"
+delete_safe "${BASE_TMP_PATH}/busybox"
--- zip-content/META-INF/com/google/android/updater-script.sh
+++ zip-content/META-INF/com/google/android/updater-script.sh
@@ -1,3 +1,7 @@
 #!/sbin/sh
 
+# SPDX-FileCopyrightText: none
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileType: SOURCE
+
 echo 'Dummy file, the real script is inside update-binary.'
