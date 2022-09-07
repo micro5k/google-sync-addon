129b13115335b410d3c33c4b943b7c642fb2b3aa
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-09-07 22:52:23 +0200

    Sync with microG unofficial installer

--- zip-content/inc/common-functions.sh
+++ zip-content/inc/common-functions.sh
@@ -1,4 +1,6 @@
 #!/sbin/sh
+# @file common-functions.sh
+# @brief A library with common functions used during flashable ZIP installation.
 
 # SPDX-FileCopyrightText: (c) 2016 ale5000
 # SPDX-License-Identifier: GPL-3.0-or-later
@@ -429,7 +431,52 @@ delete_dir_if_empty()
   fi
 }
 
-list_files()  # $1 => Folder to scan   $2 => Prefix to remove
+file_get_first_line_that_start_with()
+{
+  grep -m 1 -e "^${1:?}" -- "${2:?}" || return "${?}"
+}
+
+string_split()
+{
+  printf '%s' "${1:?}" | cut -d '|' -sf "${2:?}" || return "${?}"
+}
+
+# @description Setup an app for later installation.
+# (it automatically handle the SDK compatibility)
+#
+# @arg $1 integer Default installation setting
+# @arg $2 string Name of the app
+# @arg $3 string Filename of the app
+# @arg $4 string Folder of the app
+#
+# @exitcode 0 If installed.
+# @exitcode 1 If NOT installed.
+setup_app()
+{
+  local _install _app_conf _min_sdk _max_sdk
+  _install="${1:-0}"
+  _app_conf="$(file_get_first_line_that_start_with "${4:?}/${3:?}|" "${TMP_PATH}/files/system-apps/file-list.dat")" || ui_error "Failed to get app config for '${2}'"
+  _min_sdk="$(string_split "${_app_conf:?}" 2)" || ui_error "Failed to get min SDK for '${2}'"
+  _max_sdk="$(string_split "${_app_conf:?}" 3)" || ui_error "Failed to get max SDK for '${2}'"
+  _output_name="$(string_split "${_app_conf:?}" 4)" || ui_error "Failed to get output name for '${2}'"
+
+  if test "${API:?}" -ge "${_min_sdk:?}" && test "${API:?}" -le "${_max_sdk:-99}" && test -f "${TMP_PATH}/files/system-apps/${4:?}/${3:?}.apk"; then
+    if test "${live_setup_enabled:?}" = 'true'; then
+      choose "Do you want to install ${2:?}?" '+) Yes' '-) No'
+      if test "${?}" -eq 3; then _install='1'; else _install='0'; fi
+    fi
+
+    if test "${_install:?}" -ne 0; then
+      move_rename_file "${TMP_PATH}/files/system-apps/${4:?}/${3:?}.apk" "${TMP_PATH}/files/${4:?}/${_output_name:?}.apk" && return 0
+    fi
+  else
+    ui_debug "Skipped: ${2:?}"
+  fi
+
+  return 1
+}
+
+list_files()  # $1 => Folder to scan  $2 => Prefix to remove
 {
   test -d "$1" || return
   for entry in "$1"/*; do
