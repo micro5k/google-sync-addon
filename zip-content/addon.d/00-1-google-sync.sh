d60d25c4b545fc7338653bb6d33dc82c4daf9421
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-05-05 14:35:46 +0200

    Sync with microG unofficial installer

--- zip-content/addon.d/00-1-google-sync.sh
+++ zip-content/addon.d/00-1-google-sync.sh
@@ -3,19 +3,34 @@
 # SPDX-FileCopyrightText: none
 # SPDX-License-Identifier: CC0-1.0
 
+VANITY_NAME='Google sync add-on'
+
+_fix_tmpdir()
+{
+  # In some cases ${TMPDIR} is not set and it cause errors with HereDocs
+  if test -z "${TMPDIR-}" || test ! -w "${TMPDIR:?}"; then
+    if test -w '/tmp'; then TMPDIR='/tmp'; elif test -w '/data/local/tmp'; then TMPDIR='/data/local/tmp'; elif test -w '/postinstall/tmp'; then TMPDIR='/postinstall/tmp'; fi
+  fi
+}
+
 _init_debug_log()
 {
+  test -z "${DEBUG_LOG-}" || return
+  _fix_tmpdir
   DEBUG_LOG=0
   DEBUG_LOG_FILE=''
 
   if command 1> /dev/null -v 'getprop' && test "$(getprop 'zip.common.DEBUG_LOG' '0' || :)" = 1; then
-    if test -d "${TMPDIR:-/tmp}" && DEBUG_LOG_FILE="${TMPDIR:-/tmp}/debug-a5k.log"; then :; else test -d '/postinstall/tmp' && DEBUG_LOG_FILE='/postinstall/tmp/debug-a5k.log'; fi
-    if test -n "${DEBUG_LOG_FILE?}" && touch "${DEBUG_LOG_FILE:?}"; then DEBUG_LOG=1; fi
+    if test -n "${TMPDIR-}" && test -w "${TMPDIR:?}" && DEBUG_LOG_FILE="${TMPDIR:?}/debug-a5k.log" && touch "${DEBUG_LOG_FILE:?}"; then
+      echo 1>&2 "Writing log: ${DEBUG_LOG_FILE?}"
+      DEBUG_LOG=1
+    fi
   fi
 }
 
 _display_msg()
 {
+  _init_debug_log
   echo "${1?}"
   test "${DEBUG_LOG:?}" = 0 || echo "${1?}" 1>> "${DEBUG_LOG_FILE:?}"
 }
@@ -23,7 +38,6 @@ _display_msg()
 # NOTE: The following file come from => https://github.com/LineageOS/android_vendor_lineage/blob/HEAD/prebuilt/common/bin/backuptool.functions
 # shellcheck source=/dev/null
 command . /tmp/backuptool.functions || {
-  _init_debug_log
   _display_msg 1>&2 'ERROR: Failed to source backuptool.functions'
   # shellcheck disable=SC2317
   return 9 || exit 9
@@ -31,27 +45,30 @@ command . /tmp/backuptool.functions || {
 
 list_files()
 {
-  cat << 'EOF'
+  {
+    cat << 'EOF'
 %PLACEHOLDER-1%
 EOF
+  } || {
+    _display_msg 1>&2 'ERROR: HereDoc failed'
+    return 1
+  }
 }
 
 case "${1-}" in
   backup)
-    _init_debug_log
-    _display_msg 'Backup of Google sync add-on in progress...'
+    _display_msg "${VANITY_NAME?} - stage: ${1?}..."
     list_files | while IFS='|' read -r FILE _; do
-      if test -z "${FILE?}"; then continue; fi
+      test -n "${FILE?}" || continue
       _display_msg " ${S:?}/${FILE:?}"
       backup_file "${S:?}/${FILE:?}"
     done
     _display_msg 'Done.'
     ;;
   restore)
-    _init_debug_log
-    _display_msg 'Restore of Google sync add-on in progress...'
+    _display_msg "${VANITY_NAME?} - stage: ${1?}..."
     list_files | while IFS='|' read -r FILE REPLACEMENT; do
-      if test -z "${FILE?}"; then continue; fi
+      test -n "${FILE?}" || continue
       R=''
       test -n "${REPLACEMENT?}" && R="${S:?}/${REPLACEMENT:?}"
       test -f "${C:?}/${S:?}/${FILE:?}" && restore_file "${S:?}/${FILE:?}" "${R?}"
@@ -71,7 +88,6 @@ case "${1-}" in
     # Stub
     ;;
   *)
-    _init_debug_log
     _display_msg 1>&2 "WARNING: addon.d unknown phase => ${1-}"
     ;;
 esac
