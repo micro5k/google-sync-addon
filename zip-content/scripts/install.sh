666676136007cbb7434b982d879789ae666a96b2
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-09-12 21:52:36 +0200

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- zip-content/scripts/install.sh
+++ zip-content/scripts/install.sh
@@ -34,14 +34,19 @@ if test "${SETUP_TYPE:?}" = 'install'; then
   ui_msg 'Configuring...'
   ui_msg_empty_line
 
-  setup_app 1 '' 'Google Backup Transport 4.4' 'GoogleBackupTransport44' 'priv-app' false false
-
-  setup_app "${APP_CONTACTSSYNC:?}" 'APP_CONTACTSSYNC' 'Google Contacts Sync 12' 'GoogleContactsSyncAdapter12' 'priv-app' ||
-    setup_app "${APP_CONTACTSSYNC:?}" 'APP_CONTACTSSYNC' 'Google Contacts Sync 8' 'GoogleContactsSyncAdapter8' 'priv-app' ||
-    setup_app "${APP_CONTACTSSYNC:?}" 'APP_CONTACTSSYNC' 'Google Contacts Sync 4.4' 'GoogleContactsSyncAdapter44' 'app'
+  BACKUP_TRANSPORT_IS_OPTIONAL='true'
+  if
+    setup_app "${APP_CONTACTSSYNC:?}" 'APP_CONTACTSSYNC' 'Google Contacts Sync 12' 'GoogleContactsSyncAdapter12' 'priv-app' ||
+      setup_app "${APP_CONTACTSSYNC:?}" 'APP_CONTACTSSYNC' 'Google Contacts Sync 8' 'GoogleContactsSyncAdapter8' 'priv-app' ||
+      setup_app "${APP_CONTACTSSYNC:?}" 'APP_CONTACTSSYNC' 'Google Contacts Sync 4.4' 'GoogleContactsSyncAdapter44' 'app'
+  then
+    BACKUP_TRANSPORT_IS_OPTIONAL='false'
+  fi
 
   setup_app "${APP_CALENDARSYNC:?}" 'APP_CALENDARSYNC' 'Google Calendar Sync 6' 'GoogleCalendarSyncAdapter6' 'app' ||
     setup_app "${APP_CALENDARSYNC:?}" 'APP_CALENDARSYNC' 'Google Calendar Sync 5' 'GoogleCalendarSyncAdapter5' 'app'
+
+  setup_app 1 '' 'Google Backup Transport 4.4' 'GoogleBackupTransport44' 'priv-app' false "${BACKUP_TRANSPORT_IS_OPTIONAL:?}"
 fi
 
 if test "${SETUP_TYPE:?}" = 'install'; then
